ebe4a76229f122a3595c25cbaf4882d1f4983f19
Lrr <764444153@qq.com>
2026-07-29 10:28:47 +0800

    ci: configure automated deployment to github pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import uni from '@dcloudio/vite-plugin-uni';
 
 export default defineConfig({
   plugins: [uni()],
+  base: process.env.NODE_ENV === 'production' ? '/animal-island-uniapp/' : '/',
   server: {
-    host: true, // 监听所有地址，包括局域网 IP
-    port: 5174, // 避免与原项目 demo 的 5173 冲突
+    host: true,
+    port: 5173
   },
 });
